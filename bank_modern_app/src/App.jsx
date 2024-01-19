82d61f44eb2439def355e36491c7ca3a68fbce0b
elifguney7 <elifguney13@gmail.com>
2024-01-19 23:59:29 +0300

    css

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <div className='bg-primary w-full overflow-hidden'>
+        <h1>Hello, world!</h1>
+      </div>
     </div>
   )
 }
